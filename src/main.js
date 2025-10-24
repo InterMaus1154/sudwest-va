3ba2b00d28fe1654044042e0fd45168037d5d11e
intermaus1154 <amarco1154@gmail.com>
2025-10-24 15:24:13 +0200

    style header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import "./app.css";
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router';
+import HomePage from "@/pages/HomePage.vue";
+
+const routes = [
+  {
+    path: '/',
+    component: HomePage
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: routes,
+});
 
-export default router
+export default router;
